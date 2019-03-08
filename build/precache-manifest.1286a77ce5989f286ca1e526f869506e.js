74575e3ce6c4296c0121c5159c7952225df7d464
Edward Lombe <edward.lombe@mycosystems.com.au>
2019-03-08 15:04:06 +1100

    - Add build files to serve with github pages

---
+++ build/precache-manifest.1286a77ce5989f286ca1e526f869506e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/js-logo.dd60b11e.svg"
+  },
+  {
+    "revision": "f37c1220dfd248969ac3",
+    "url": "/static/css/main.70401ef9.chunk.css"
+  },
+  {
+    "revision": "4da9db0ca0c0227313b8f38ad99688c1",
+    "url": "/static/media/open-source-contribution.4da9db0c.png"
+  },
+  {
+    "revision": "7a706c257f7030d609f9",
+    "url": "/static/js/1.7a706c25.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c42995e9ac7af733c625767ed25c800",
+    "url": "/static/media/node-logo.8c42995e.png"
+  },
+  {
+    "revision": "cf169f32a40870af410554a960081e5f",
+    "url": "/static/media/npm-logo.cf169f32.png"
+  },
+  {
+    "revision": "5c69e565f1ae6d7717fb3ecd2be004ad",
+    "url": "/static/media/angular-logo.5c69e565.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react-logo.4009eef3.svg"
+  },
+  {
+    "revision": "0459de24f638da57a44266b0d827ec1f",
+    "url": "/static/media/elm-logo.0459de24.svg"
+  },
+  {
+    "revision": "525f317fe9e90927edf7694d88ce42c8",
+    "url": "/static/media/html5-logo.525f317f.svg"
+  },
+  {
+    "revision": "f37c1220dfd248969ac3",
+    "url": "/static/js/main.f37c1220.chunk.js"
+  },
+  {
+    "revision": "96621d3c37d96ad3bf792fcc848d912f",
+    "url": "/static/media/webpack-logo.96621d3c.svg"
+  },
+  {
+    "revision": "86d6b790cda76a3ef186515a2667a284",
+    "url": "/static/media/ts-logo.86d6b790.png"
+  },
+  {
+    "revision": "846547b4a89b2a39f785dabefd53e499",
+    "url": "/static/media/lbg.846547b4.jpg"
+  },
+  {
+    "revision": "d0a62d3824f7c3f5d65bc45e0a25b387",
+    "url": "/static/media/headshot.d0a62d38.jpg"
+  },
+  {
+    "revision": "ce88b78e767a157a59e5f06a7fc754cf",
+    "url": "/static/media/iPhone-Images-A1.ce88b78e.jpg"
+  },
+  {
+    "revision": "87f4cf856e21243724d2295b142553cf",
+    "url": "/static/media/iPhone-Images.87f4cf85.jpg"
+  },
+  {
+    "revision": "ec00cdb7d9c35dc6477a8cbab41dabae",
+    "url": "/static/media/sale-note-0.ec00cdb7.png"
+  },
+  {
+    "revision": "b76aa5b2e4be49263041cf826b6f02d7",
+    "url": "/static/media/sale-note-1.b76aa5b2.png"
+  },
+  {
+    "revision": "7a706c257f7030d609f9",
+    "url": "/static/css/1.f77ca94f.chunk.css"
+  },
+  {
+    "revision": "4e54f5d980912e58ef016b8146bf1010",
+    "url": "/index.html"
+  }
+];
